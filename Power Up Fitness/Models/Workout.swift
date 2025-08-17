be2cfeb430d3e478fb155affa9d160127ceb4c6e
Lucas Hunter <lphunter@usc.edu>
2025-08-17 00:41:00 -0700

    added workout models

---
+++ Power Up Fitness/Models/Workout.swift
@@ -0,0 +1,120 @@
+//
+//  Workout.swift
+//  Power Up Fitness
+//
+//  Created by Lucas Hunter on 8/17/25.
+//
+
+import Foundation
+
+enum WorkoutType: Identifiable, Codable {
+    case strength(StrengthWorkout)
+    case endurance(EnduranceWorkout)
+    case highIntensityCardio(HighIntensityCardioWorkout)
+}
+
+struct StrengthWorkout: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var date: Date
+    var exercises: [Exercise]
+}
+
+struct Exercise: Codable {
+    var name: String
+    var sets: Int
+    var reps: Int
+    var weight: Double 
+}
+
+struct EnduranceWorkout: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var date: Date
+    var duration: Int // in minutes
+}
+
+enum HighIntensityCardioWorkout: Identifiable, Codable {
+    var id = UUID()
+    var date: Date
+    var type: HICWorkoutType
+    
+}
+
+enum HICWorkoutType: Identifiable, Codable {
+    case aerobicAnaerobic(HICAerobicAnaerobic)
+    case generalConditioning(HICGeneralConditioning)
+    case powerDevelopment(HICPowerDevelopment)
+    
+    var id: UUID {
+        switch self {
+        case .aerobicAnaerobic(let workout):
+            return workout.id
+        case .generalConditioning(let workout):
+            return workout.id
+        case .powerDevelopment(let workout):
+            return workout.id
+        }
+    }
+}
+
+struct HICAerobicAnaerobic: Codable {
+    var id = UUID()
+}
+
+struct HICGeneralConditioning: Codable {
+    var id = UUID()
+}
+
+struct HICPowerDevelopment: Codable {
+    var id = UUID()
+}
+
+enum HICAerobicAnaerobicPreset: String, Codable, CaseIterable {
+    case connaughtRange10to1s = "Connaught Range 10 to 1s"
+    case fast5 = "Fast 5"
+    case sixHundredMResets = "600M Resets"
+    case heavyBagResets = "Heavy Bag Resets"
+    case indoorPowerIntervals = "Indoor Power Intervals"
+    case sledgeDrill = "Sledge Drill"
+    case boo = "BOO"
+    case booII = "BOO II"
+    case fobbitIntervals = "Fobbit Intervals"
+    case shortHills = "Short Hills"
+    case oxygenDebt101 = "Oxygen Debt 101"
+    case speedEnduranceLadders = "Speed-Endurance Ladders"
+    case meatEater = "Meat Eater"
+    case meatEaterII = "Meat Eater II"
+    case disarmed = "Disarmed"
+    case standardIssueHills = "Standard Issue Hills"
+    case apexHills = "Apex Hills"
+    case bloodyLungs = "Bloody Lungs"
+    case bloodyLungsII = "Bloody Lungs II"
+    case anaerobicCapacity = "Anaerobic Capacity"
+    case pepperPot = "Pepper Pot"
+    case buffaloLaps = "Buffalo Laps"
+    case meatEaterIII = "Meat Eater III"
+    case devilsTrinity = "Devil's Trinity - Combat Conditioning Circuit"
+}
+
+enum HICGeneralConditioningPreset: String, Codable, CaseIterable {
+    case gc1BeatYourFace = "GC# 1 aka Beat Your Face"
+    case gc2 = "GC# 2"
+    case gc3BrigRat = "GC# 3 aka Brig Rat"
+    case gc4 = "GC# 4"
+    case gc5 = "GC #5"
+    case gc6 = "GC #6"
+    case gc7 = "GC #7"
+    case gc8 = "GC #8"
+    case gc9 = "GC #9"
+    case gc10 = "GC #10"
+    case gc11OutsideTheWire = "GC #11 aka Outside the Wire"
+    case gc12 = "GC #12"
+}
+
+enum HICPowerDevelopmentPreset: String, Codable, CaseIterable {
+    case bwPlyometricPower = "BW Plyometric Power"
+    case powerComplex = "Power Complex"
+    case kineticConditioning = "Kinetic Conditioning"
+    case transitionComplex = "Transition Complex"
+}
